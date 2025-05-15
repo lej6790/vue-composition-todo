daea3ed6e74d8eecffc5b6848227df8d43901e82
wonjong <lej6790@gmail.com>
2025-05-15 21:23:29 +0900

    1. 할일 관리  프로젝트 생성 2. 할일 관리 헤더, 인풋, 리스트 컴포넌트 구현 3. 목록 표시 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TodoHeader></TodoHeader>
+  <TodoInput></TodoInput>
+  <TodoList></TodoList>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from '@/components/TodoHeader.vue';
+import TodoInput from '@/components/TodoInput.vue';
+import TodoList from '@/components/TodoList.vue';
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TodoHeader,
+    TodoInput,
+    TodoList
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
